5a9c3c5b22a0efade5a7c6ef7e87daeb96866db9
Genilson Ferreira <Genilson Ferreira>
2016-05-11 17:38:41 -0300

    pares ordenado ok - falta fazer o fecho simetrico

---
+++ MDRelacaoSimetrica/MDRelacaoSimetrica/views/frmHScreen.cs
@@ -0,0 +1,324 @@
+﻿/**
+ * **********************************************************************************************************
+ * @copyright   (c) 2016 - g3ferreira      
+ * @brief      PAA Merge Sort Trabalho
+ *                                                                                                    
+ * @details                                                                                      
+ *                                                                                                    
+ * @author      Genilson Ferreira <gr.ferreira@live.com>                                                                
+ * @since       Abr 12, 2016                                                                     
+ *                                                                                                    
+ * @sa           
+ **********************************************************************************************************
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using System.Xml;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net.Sockets;
+using MDRelacaoSimetrica.controllers;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MDRelacaoSimetrica
+{
+    public partial class frmHomeScreen : Form
+    {
+        public static System.Timers.Timer _timer = new System.Timers.Timer();
+
+        public static Stopwatch cronometroWatch = new Stopwatch();
+        public static string timeElapsed = string.Empty;
+        public Thread threadMergeSort;
+
+        public frmHomeScreen()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void atualiazarCronometroView(string timeElapsed)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(atualiazarCronometroView), new object[] { timeElapsed });
+                return;
+            }
+
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            //txtPathVMI.Text = fbd.SelectedPath;
+            // string[] files = Directory.GetFiles(fbd.SelectedPath);
+            //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+            //  tagXml.generateReport(docXML, "REPORT","001"); //gera o relatorio
+            // tagXml.AtualizaValorNodo(docXML,"COMPONENT", "777%");
+            // tagXml.setValueQR1Report(docXML, "COMPONENT", 0, "350%"); change value for QR1 tag
+
+            // fillDataGrid();
+
+            //  RepositoryWatcher.Run(@"C:\config-autovmi\pasta");
+
+        }
+
+        private void btnPathAOI_Click(object sender, EventArgs e)
+        {
+            txtN.Text = selectPath();
+        }
+
+        public String selectPath()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+
+            DialogResult result = openFileDialog.ShowDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            return openFileDialog.FileName;
+        }
+
+        public string selectPathArquivoDesordenado()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            return fbd.SelectedPath;
+        }
+
+        private void btnPathVMI_Click(object sender, EventArgs e)
+        {
+            txtM.Text = selectPathArquivoDesordenado();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtLog1.Text = string.Empty;
+            txtLog2.Text = string.Empty;
+            //simuladResultados();
+            if (!(validarParametros(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text))))
+            {
+                if( Convert.ToInt32(txtQ.Text) ==   (Math.Pow(Convert.ToInt32(txtM.Text),2)))
+                {
+                    lblResultado.Text = "1 - Pares Ordenado Simétricos";
+                }
+
+                simetria(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text));
+            }
+
+        }
+        public bool validarParametros(int n, int m, int q)
+        {
+
+            bool ret = false;
+            if (m < 0)
+            {
+                ret = true;
+                MessageBox.Show("M deve ser maior ou igual a 0");
+
+            }
+            if (n > (Math.Pow(10, 2)))
+            {
+                ret = true;
+                MessageBox.Show("N deve ser menor ou igual a:   " + Math.Pow(10, 2));
+            }
+            if (q < 1)
+            {
+                ret = true;
+                MessageBox.Show("Q deve ser maior ou igual a 1");
+            }
+            if (q > (Math.Pow(10, 4)))
+            {
+                ret = true;
+                MessageBox.Show("N deve ser menor ou igual a:    " + Math.Pow(10, 4));
+            }
+            if (q > (Math.Pow(m, 2)))
+            {
+                ret = true;
+                MessageBox.Show("Q deve ser menor ou igual a:   " + Math.Pow(m, 2));
+            }
+
+            return ret;
+        }
+        public void simetria(int n, int m, int q)
+        {
+            
+            int m2 = m;
+            int n2 = n;
+            if (q <= m2)
+            {
+                for (int i = n2; i <= q; i++)
+                {
+                    txtLog1.Text += n2 + "-" + n2;
+                    txtLog1.Text += Environment.NewLine;
+
+                    n2++;
+                }
+            }
+            else
+            {
+                for (int i = n2; i <= m; i++)
+                {
+                    txtLog1.Text += n2 + "-" + n2;
+                    txtLog1.Text += Environment.NewLine;
+
+                    n2++;
+                }
+                n2 = n;
+                m2 = m;
+                for (int i = n2; i < m; i++)
+                {
+                    txtLog1.Text += n2 + "-" + (i + 1);
+                    txtLog1.Text += Environment.NewLine;
+
+                }
+
+                n2 = n;
+                m2 = m;
+                for (int i = n2; i <= (q - m); i++)
+                {
+                    if (!(i > m))
+                    {
+                        if (!(m2 == i))
+                        {
+                            txtLog1.Text += m2 + "-" + i;
+                            txtLog1.Text += Environment.NewLine;
+                        }
+                    }
+                    else
+                    {
+                        if (!((m2-1) == (i-m)))
+                        {
+                            txtLog1.Text += (m2 - 1) + "-" + (i - m);
+                            txtLog1.Text += Environment.NewLine;
+                        }
+                    }
+                }
+
+
+            }
+
+
+        }
+
+        public void recursivo(int n, int m)
+        {
+            int j = n;
+            for (int i = n; i <= m; i++)
+            {
+                txtLog1.Text += n + "-" + j;
+                txtLog1.Text += Environment.NewLine;
+                j++;
+            }
+        }
+
+        public void CreateMyMultilineTextBox()
+        {
+            txtLog1.Multiline = true;
+            txtLog1.ScrollBars = ScrollBars.Vertical;
+            txtLog1.AcceptsReturn = true;
+            txtLog1.AcceptsTab = true;
+            txtLog1.WordWrap = true;
+            txtLog2.Multiline = true;
+            txtLog2.ScrollBars = ScrollBars.Vertical;
+            txtLog2.AcceptsReturn = true;
+            txtLog2.AcceptsTab = true;
+            txtLog2.WordWrap = true;
+
+        }
+
+
+
+
+
+
+
+        public void disableEnableFieldsHScreen(bool enableDisable)
+        {
+            txtN.Enabled = enableDisable;
+            txtM.Enabled = enableDisable;
+
+        }
+
+        public bool validateParameters()
+        {
+
+            if (!(txtN.Text.Equals(string.Empty) || txtN.Text.Equals(string.Empty)))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void frmHomeScreen_Load(object sender, EventArgs e)
+        {
+            CreateMyMultilineTextBox();
+            txtLog1.Height = 350;
+            txtLog2.Height = 350;
+
+            txtN.Text = @"1";
+            txtM.Text = @"3";
+            txtQ.Text = "2";
+            lblResultado.Text = string.Empty;
+
+
+        }
+
+        public void simuladResultados()
+        {
+            lblResultado.Text = "0 - Fecho Não Simétrico";
+            txtLog1.Text += "1,1";
+            txtLog1.Text += Environment.NewLine;
+            txtLog1.Text += "1,2";
+            txtLog1.Text += Environment.NewLine;
+            txtLog2.Text += "1,1";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "1,2";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "1,3";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "2,1";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "2,2";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "2,3";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "3,1";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "3,2";
+            txtLog2.Text += Environment.NewLine;
+            txtLog2.Text += "3,3";
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ MDRelacaoSimetrica/MDRelacaoSimetrica/views/frmHScreen.designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MDRelacaoSimetrica
+{
+    partial class frmHomeScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHomeScreen));
+            this.panelHomeScreen = new System.Windows.Forms.Panel();
+            this.txtLog1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtQ = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.txtLog2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.panelHomeScreen.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHomeScreen
+            // 
+            this.panelHomeScreen.BackColor = System.Drawing.SystemColors.Menu;
+            this.panelHomeScreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelHomeScreen.Controls.Add(this.lblResultado);
+            this.panelHomeScreen.Controls.Add(this.label5);
+            this.panelHomeScreen.Controls.Add(this.label1);
+            this.panelHomeScreen.Controls.Add(this.txtLog2);
+            this.panelHomeScreen.Controls.Add(this.txtLog1);
+            this.panelHomeScreen.Controls.Add(this.label6);
+            this.panelHomeScreen.Controls.Add(this.txtQ);
+            this.panelHomeScreen.Controls.Add(this.btnStart);
+            this.panelHomeScreen.Controls.Add(this.panel2);
+            this.panelHomeScreen.Controls.Add(this.label3);
+            this.panelHomeScreen.Controls.Add(this.txtM);
+            this.panelHomeScreen.Controls.Add(this.label2);
+            this.panelHomeScreen.Controls.Add(this.txtN);
+            this.panelHomeScreen.Location = new System.Drawing.Point(45, 49);
+            this.panelHomeScreen.Name = "panelHomeScreen";
+            this.panelHomeScreen.Size = new System.Drawing.Size(755, 589);
+            this.panelHomeScreen.TabIndex = 1;
+            // 
+            // txtLog1
+            // 
+            this.txtLog1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtLog1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog1.Location = new System.Drawing.Point(45, 223);
+            this.txtLog1.Name = "txtLog1";
+            this.txtLog1.ReadOnly = true;
+            this.txtLog1.Size = new System.Drawing.Size(319, 26);
+            this.txtLog1.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(520, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 22);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Qtd.Pares Ordenados (Q)";
+            // 
+            // txtQ
+            // 
+            this.txtQ.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQ.Location = new System.Drawing.Point(524, 39);
+            this.txtQ.Name = "txtQ";
+            this.txtQ.Size = new System.Drawing.Size(190, 26);
+            this.txtQ.TabIndex = 20;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(315, 74);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 36);
+            this.btnStart.TabIndex = 15;
+            this.btnStart.Text = "Executar";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(46, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(669, 26);
+            this.panel2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(-1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Resultados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(262, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nr. Final (M)";
+            // 
+            // txtM
+            // 
+            this.txtM.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtM.Location = new System.Drawing.Point(266, 39);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(240, 26);
+            this.txtM.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nr. Inicial (N)";
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN.Location = new System.Drawing.Point(46, 39);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(200, 26);
+            this.txtN.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(844, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // txtLog2
+            // 
+            this.txtLog2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtLog2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog2.Location = new System.Drawing.Point(392, 223);
+            this.txtLog2.Name = "txtLog2";
+            this.txtLog2.ReadOnly = true;
+            this.txtLog2.Size = new System.Drawing.Size(322, 26);
+            this.txtLog2.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 22);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Pares Ordenados";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(388, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 22);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Fecho Simétrico";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblResultado.Location = new System.Drawing.Point(276, 163);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(210, 22);
+            this.lblResultado.TabIndex = 26;
+            this.lblResultado.Text = "0 - Par Ord. Não Simétrico";
+            // 
+            // frmHomeScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(844, 667);
+            this.Controls.Add(this.panelHomeScreen);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmHomeScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HOME SCREEN ";
+            this.Load += new System.EventHandler(this.frmHomeScreen_Load);
+            this.panelHomeScreen.ResumeLayout(false);
+            this.panelHomeScreen.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHomeScreen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtQ;
+        private System.Windows.Forms.TextBox txtLog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLog2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblResultado;
+
+
+
+
+
+    }
+}
+
