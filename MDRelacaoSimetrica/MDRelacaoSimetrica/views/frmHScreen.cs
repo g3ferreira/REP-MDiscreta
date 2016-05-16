9cb4f9ec9b65eb7e5801b9c20767f981423d20cb
Genilson Ferreira <Genilson Ferreira>
2016-05-16 12:28:37 -0300

    versao completa ok

--- MDRelacaoSimetrica/MDRelacaoSimetrica/views/frmHScreen.cs
+++ MDRelacaoSimetrica/MDRelacaoSimetrica/views/frmHScreen.cs
@@ -1,12 +1,12 @@
 ﻿/**
  * **********************************************************************************************************
- * @copyright   (c) 2016 - g3ferreira      
- * @brief      PAA Merge Sort Trabalho
+ * @copyright   (c) 2016 - GitHub: g3ferreira      
+ * @brief     Simetria entre pares ordenados
  *                                                                                                    
  * @details                                                                                      
  *                                                                                                    
  * @author      Genilson Ferreira <gr.ferreira@live.com>                                                                
- * @since       Abr 12, 2016                                                                     
+ * @since       Mai 16, 2016                                                                     
  *                                                                                                    
  * @sa           
  **********************************************************************************************************
@@ -34,99 +34,140 @@ namespace MDRelacaoSimetrica
 {
     public partial class frmHomeScreen : Form
     {
-        public static System.Timers.Timer _timer = new System.Timers.Timer();
-
-        public static Stopwatch cronometroWatch = new Stopwatch();
-        public static string timeElapsed = string.Empty;
-        public Thread threadMergeSort;
 
         public frmHomeScreen()
         {
             InitializeComponent();
         }
 
-
-
-        public void atualiazarCronometroView(string timeElapsed)
-        {
-            if (InvokeRequired)
-            {
-                this.Invoke(new Action<string>(atualiazarCronometroView), new object[] { timeElapsed });
-                return;
-            }
-
-        }
-
-
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            DialogResult result = fbd.ShowDialog();
-            //txtPathVMI.Text = fbd.SelectedPath;
-            // string[] files = Directory.GetFiles(fbd.SelectedPath);
-            //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
-        }
-
-        private void button1_Click_2(object sender, EventArgs e)
-        {
-
-            //  tagXml.generateReport(docXML, "REPORT","001"); //gera o relatorio
-            // tagXml.AtualizaValorNodo(docXML,"COMPONENT", "777%");
-            // tagXml.setValueQR1Report(docXML, "COMPONENT", 0, "350%"); change value for QR1 tag
-
-            // fillDataGrid();
-
-            //  RepositoryWatcher.Run(@"C:\config-autovmi\pasta");
-
-        }
-
-        private void btnPathAOI_Click(object sender, EventArgs e)
-        {
-            txtN.Text = selectPath();
-        }
-
-        public String selectPath()
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.CheckFileExists = true;
-
-            DialogResult result = openFileDialog.ShowDialog();
-            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            return openFileDialog.FileName;
-        }
-
-        public string selectPathArquivoDesordenado()
-        {
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            DialogResult result = fbd.ShowDialog();
-            return fbd.SelectedPath;
-        }
-
-        private void btnPathVMI_Click(object sender, EventArgs e)
-        {
-            txtM.Text = selectPathArquivoDesordenado();
-        }
-
         private void btnStart_Click(object sender, EventArgs e)
         {
             txtLog1.Text = string.Empty;
             txtLog2.Text = string.Empty;
-            //simuladResultados();
+
             if (!(validarParametros(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text))))
             {
-                if( Convert.ToInt32(txtQ.Text) ==   (Math.Pow(Convert.ToInt32(txtM.Text),2)))
+                if ((Convert.ToInt32(txtQ.Text) == (Math.Pow(Convert.ToInt32(txtM.Text), 2))) || Convert.ToInt32(txtQ.Text) == 1)
                 {
-                    lblResultado.Text = "1 - Pares Ordenado Simétricos";
+                    lblResultado.ForeColor = Color.Green;
+                    lblResultado.Text = "1 - Pares Ordenados Simétricos";
+                    criarParesOrdenados(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text));
                 }
+                else
+                {
+                    lblResultado.ForeColor = Color.Red;
+                    lblResultado.Text = "0 - Pares Ordenados Não Simétricos";                    
+                    criarParesOrdenados(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text));
+                    txtLog2.Text = txtLog1.Text;
+                    simetria();
+                }
+            }
+        }
 
-                simetria(Convert.ToInt32(txtN.Text), Convert.ToInt32(txtM.Text), Convert.ToInt32(txtQ.Text));
+
+        /*
+         * Metodo que irá exibir a simetria dos pares ordenados na tela 
+         * 
+         * 1 - pega os pares ordenados e remove os espacos em branco
+         * 2 - criar os pares ordenados simetricos a partir do pares ordenados
+         * 3 - remover todos os pares repetidos da lista de pares simetricos. Compara-se a os pares ordenados com os pares ordenados simetricos se os pares forem iguais, remove-se um.
+         * 4 - listar os pares ordenados simetricos na tela
+         * 
+         */
+
+        public void simetria()
+        {
+            string[] paresOrdenados = txtLog1.Text.Split();
+            List<string> listaPares = new List<string>();
+            List<string> listaParesSimetricos = new List<string>();
+
+            listaPares = listarParesOrdenados(paresOrdenados);
+            listaParesSimetricos = listarParesSimetricos(listaPares);
+            listaParesSimetricos = removerParesSimetricosIguais(listaPares, listaParesSimetricos);
+            listarParesSimetricosView(listaParesSimetricos);
+
+        }
+
+        //metodo que monta os pares ordenados 
+        public List<string> listarParesOrdenados(string[] paresOrdenados)
+        {
+            List<string> listaParesOrdenados = new List<string>();
+            for (int i = 0; i < paresOrdenados.Length; i++)
+            {
+                if (!(paresOrdenados[i].Equals("")))
+                {
+                    listaParesOrdenados.Add(paresOrdenados[i]);
+                }
+            }
+
+            return listaParesOrdenados;
+        }
+
+        //metodo que monta os pares ordenados simetricos a partir dos pares ordenados
+        public List<string> listarParesSimetricos(List<string> listaParesOrdenados)
+        {
+            string parSimetrico = string.Empty;
+            List<string> listaParesSimetricos = new List<string>();
+            foreach (var item in listaParesOrdenados)
+            {
+                string[] parSplitArray = item.Split('-');
+                parSimetrico += parSplitArray[1] + "-";
+                parSimetrico += parSplitArray[0];
+                listaParesSimetricos.Add(parSimetrico);
+                parSimetrico = string.Empty;
+            }
+
+            return listaParesSimetricos;
+        }
+
+        // metodo que remove os pares ordenados repetidos  
+        public List<string> removerParesSimetricosIguais(List<string> listaParesOrdenados, List<string> listaParesSimetricos)
+        {
+
+            for (int i = 0; i < listaParesOrdenados.Count; i++)
+            {
+                if (listaParesSimetricos.Contains(listaParesOrdenados[i]))
+                {
+                    listaParesSimetricos.Remove(listaParesOrdenados[i]);
+                }
+            }
+
+            return listaParesSimetricos;
+
+        }
+
+        //metodo para exibir os pares ordenados simétricos na tela
+        public void listarParesSimetricosView(List<string> listaParesSimetricos)
+        {
+            foreach (var item in listaParesSimetricos)
+            {
+                txtLog2.Text += item;
+                txtLog2.Text += Environment.NewLine;
             }
 
         }
+
+        //metodo que monta os pares ordenados e os exibe na tela
+        public void montarParOrdenado(int x, List<int> listaN, int quantidadeCombinacoes)
+        {
+            int y = 0; ;
+            for (int i = 0; i <= quantidadeCombinacoes; i++)
+            {
+                if (i >= listaN.Count) break;
+                y = listaN[i];
+                if (!(x == y))
+                {
+                    Console.WriteLine("par: " + x + "-" + y);
+                    txtLog2.Text += x + "-" + y;
+                    txtLog2.Text += Environment.NewLine;
+                }
+
+            }
+        }
+        
+        //metodo para validar parametros 
         public bool validarParametros(int n, int m, int q)
         {
-
             bool ret = false;
             if (m < 0)
             {
@@ -154,79 +195,46 @@ namespace MDRelacaoSimetrica
                 ret = true;
                 MessageBox.Show("Q deve ser menor ou igual a:   " + Math.Pow(m, 2));
             }
-
+            lblResultado.Text = string.Empty;
             return ret;
         }
-        public void simetria(int n, int m, int q)
+
+        //criar pares ordenados não simetricos e exibir os mesmos na tela
+        public int parOrdenadoNaoSimetrico(int x, List<int> listaN, int limitePares, int contPares)
         {
-            
-            int m2 = m;
-            int n2 = n;
-            if (q <= m2)
+            int y = 0;
+            for (int i = 0; i <= listaN.Count; i++)
             {
-                for (int i = n2; i <= q; i++)
+                if (contPares <= limitePares)
                 {
-                    txtLog1.Text += n2 + "-" + n2;
+                    if (i >= listaN.Count) break;
+                    y = listaN[i];
+                    txtLog1.Text += x + "-" + y;
                     txtLog1.Text += Environment.NewLine;
-
-                    n2++;
+                    contPares++;
                 }
-            }
-            else
-            {
-                for (int i = n2; i <= m; i++)
-                {
-                    txtLog1.Text += n2 + "-" + n2;
-                    txtLog1.Text += Environment.NewLine;
+            } 
 
-                    n2++;
-                }
-                n2 = n;
-                m2 = m;
-                for (int i = n2; i < m; i++)
-                {
-                    txtLog1.Text += n2 + "-" + (i + 1);
-                    txtLog1.Text += Environment.NewLine;
-
-                }
-
-                n2 = n;
-                m2 = m;
-                for (int i = n2; i <= (q - m); i++)
-                {
-                    if (!(i > m))
-                    {
-                        if (!(m2 == i))
-                        {
-                            txtLog1.Text += m2 + "-" + i;
-                            txtLog1.Text += Environment.NewLine;
-                        }
-                    }
-                    else
-                    {
-                        if (!((m2-1) == (i-m)))
-                        {
-                            txtLog1.Text += (m2 - 1) + "-" + (i - m);
-                            txtLog1.Text += Environment.NewLine;
-                        }
-                    }
-                }
-
-
-            }
-
-
+            return contPares;
         }
 
-        public void recursivo(int n, int m)
+        //pegar os parametros de N, M e Q e criar os pares ordenados
+        public void criarParesOrdenados(int n, int m, int q)
         {
-            int j = n;
+            int contParOrdenado = 1;
+            List<int> listaNumeros = new List<int>();
             for (int i = n; i <= m; i++)
             {
-                txtLog1.Text += n + "-" + j;
-                txtLog1.Text += Environment.NewLine;
-                j++;
+                listaNumeros.Add(i);
+
             }
+            for (int i = 0; i < listaNumeros.Count; i++)
+            {
+                if (contParOrdenado <= q) contParOrdenado = parOrdenadoNaoSimetrico(listaNumeros[i], listaNumeros, q, contParOrdenado);
+
+            }
+
+
         }
 
         public void CreateMyMultilineTextBox()
@@ -243,12 +251,6 @@ namespace MDRelacaoSimetrica
             txtLog2.WordWrap = true;
 
         }
-
-
-
-
-
-
 
         public void disableEnableFieldsHScreen(bool enableDisable)
         {
@@ -279,7 +281,7 @@ namespace MDRelacaoSimetrica
             txtLog2.Height = 350;
 
             txtN.Text = @"1";
-            txtM.Text = @"3";
+            txtM.Text = @"4";
             txtQ.Text = "2";
             lblResultado.Text = string.Empty;
 
